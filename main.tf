809eda099afb4758c7f21d734fcc9b2a62a14759
Jakub Hlavačka <jakubhlavacka123@gmail.com>
2022-12-29 12:09:48 +0100

    redis persistent volume claim

--- main.tf
+++ main.tf
@@ -17,7 +17,9 @@ module "my_namespaces" {
 
 module "my_secrets" {
   source = "./secrets"
+
   diplomovka_namespace_name = module.my_namespaces.namespace_name
+  
   depends_on = [
     module.my_namespaces
   ]
@@ -32,8 +34,11 @@ module "my_persistent_volumes" {
 
 module "my_persistent_volume_claims" {
   source = "./persistent_volume_claims"
+
   diplomovka_namespace_name = module.my_namespaces.namespace_name
   minio_pv_name = module.my_persistent_volumes.minio_pv_name
+  redis_pv_name = module.my_persistent_volumes.redis_pv_name
+
   depends_on = [
     module.my_persistent_volumes
   ]
@@ -41,9 +46,11 @@ module "my_persistent_volume_claims" {
 
 module "my_deployments" {
   source = "./deployments"
+
   diplomovka_namespace_name = module.my_namespaces.namespace_name
   persistent_volume_claim_name = module.my_persistent_volume_claims.minio_pvc_name
   minio_secret_name = module.my_secrets.minio_secret_name
+
   depends_on = [
     module.my_persistent_volume_claims,
     module.my_secrets
@@ -52,8 +59,10 @@ module "my_deployments" {
 
 module "my-services" {
   source = "./services"
+
   diplomovka_namespace_name = module.my_namespaces.namespace_name
   pods_name = module.my_deployments.pods_name
+
   depends_on = [
     module.my_deployments
   ]
@@ -61,8 +70,10 @@ module "my-services" {
 
 module "my-ingress" {
   source = "./ingress"
+
   diplomovka_namespace_name = module.my_namespaces.namespace_name
   service_name = module.my-services.service_name
+
   depends_on = [
     module.my-services
   ]
--- persistent_volume_claims/main.tf
+++ persistent_volume_claims/main.tf
@@ -8,7 +8,12 @@ variable "minio_pv_name" {
   default = "minio-pv"
 }
 
-resource "kubernetes_persistent_volume_claim_v1" "minio-pvc" {
+variable "redis_pv_name" {
+  type = string
+  default = "redis-pv"
+}
+
+resource "kubernetes_persistent_volume_claim_v1" "minio_pvc" {
   metadata {
     name = "minio-pvc"
     labels = {
@@ -29,6 +34,31 @@ resource "kubernetes_persistent_volume_claim_v1" "minio-pvc" {
   }
 }
 
+resource "kubernetes_persistent_volume_claim_v1" "redis_pvc" {
+  metadata {
+    name = "redis-pvc"
+    labels = {
+      app = "redis-pvc"
+    }
+    namespace = var.diplomovka_namespace_name
+  }
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+    volume_name = var.redis_pv_name
+    storage_class_name = "hostpath"
+  }
+}
+
 output "minio_pvc_name" {
-  value = kubernetes_persistent_volume_claim_v1.minio-pvc.metadata.0.name
+  value = kubernetes_persistent_volume_claim_v1.minio_pvc.metadata.0.name
+}
+
+output "redis_pvc_name" {
+  value = kubernetes_persistent_volume_claim_v1.redis_pvc.metadata.0.name
 }
