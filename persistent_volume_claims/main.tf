ab4f9bafc8ab85dd07fd6981cbe40761cf004008
Jakub Hlavačka <jakubhlavacka123@gmail.com>
2022-12-29 17:47:05 +0100

    variables.tf in persistent_volume_claims

--- persistent_volume_claims/main.tf
+++ persistent_volume_claims/main.tf
@@ -1,57 +1,42 @@
-variable "diplomovka_namespace_name" {
-  type = string
-  default = "diplomovka"
-}
-
-variable "minio_pv_name" {
-  type = string
-  default = "minio-pv"
-}
-
-variable "redis_pv_name" {
-  type = string
-  default = "redis-pv"
-}
-
 resource "kubernetes_persistent_volume_claim_v1" "minio_pvc" {
   metadata {
-    name = "minio-pvc"
+    name = var.minio_pvc_name
     labels = {
-      app = "minio-pvc"
+      app = var.minio_pvc_name
     }
     namespace = var.diplomovka_namespace_name
   }
 
   spec {
-    access_modes = ["ReadWriteOnce"]
+    access_modes = var.minio_access_modes
     resources {
       requests = {
-        storage = "1Gi"
+        storage = var.minio_pvc_storage
       }
     }
     volume_name = var.minio_pv_name
-    storage_class_name = "hostpath"
+    storage_class_name = var.minio_storage_class_name
   }
 }
 
 resource "kubernetes_persistent_volume_claim_v1" "redis_pvc" {
   metadata {
-    name = "redis-pvc"
+    name = var.redis_pvc_name
     labels = {
-      app = "redis-pvc"
+      app = var.redis_pvc_name
     }
     namespace = var.diplomovka_namespace_name
   }
 
   spec {
-    access_modes = ["ReadWriteOnce"]
+    access_modes = var.redis_access_modes
     resources {
       requests = {
-        storage = "1Gi"
+        storage = var.redis_pvc_storage
       }
     }
     volume_name = var.redis_pv_name
-    storage_class_name = "hostpath"
+    storage_class_name = var.redis_storage_class_name
   }
 }
 
---
+++ persistent_volume_claims/variables.tf
@@ -0,0 +1,54 @@
+variable "diplomovka_namespace_name" {
+  type = string
+  default = "diplomovka"
+}
+
+variable "minio_pv_name" {
+  type = string
+  default = "minio-pv"
+}
+
+variable "minio_pvc_name" {
+  type = string
+  default = "minio-pvc"
+}
+
+variable "minio_access_modes" {
+  type = list(string)
+  default = ["ReadWriteOnce"]
+}
+
+variable "minio_pvc_storage" {
+  type = string
+  default = "1Gi"
+}
+
+variable "minio_storage_class_name" {
+  type = string
+  default = "hostpath"
+}
+
+variable "redis_pv_name" {
+  type = string
+  default = "redis-pv"
+}
+
+variable "redis_pvc_name" {
+  type = string
+  default = "redis-pvc"
+}
+
+variable "redis_access_modes" {
+  type = list(string)
+  default = ["ReadWriteOnce"]
+}
+
+variable "redis_pvc_storage" {
+  type = string
+  default = "1Gi"
+}
+
+variable "redis_storage_class_name" {
+  type = string
+  default = "hostpath"
+}
