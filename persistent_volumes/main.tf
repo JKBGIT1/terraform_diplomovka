df01fa6b6181acd4fe6b8b821395107c2b5bcb23
Jakub Hlavačka <jakubhlavacka123@gmail.com>
2022-12-29 17:55:53 +0100

    variables.tf in persistent_volumes

--- persistent_volumes/main.tf
+++ persistent_volumes/main.tf
@@ -20,18 +20,18 @@ resource "kubernetes_persistent_volume_v1" "minio_pv" {
 
 resource "kubernetes_persistent_volume_v1" "redis_pv" {
   metadata {
-    name = "redis-pv"
+    name = var.redis_pv_name
   }
 
   spec {
     capacity = {
-      storage = "2Gi"
+      storage = var.redis_pv_storage
     }
-    access_modes = ["ReadWriteOnce"]
-    storage_class_name = "hostpath"
+    access_modes = var.redis_pv_access_modes
+    storage_class_name = var.redis_pv_storage_class_name
     persistent_volume_source {
       host_path {
-        path = "/mnt/redis"
+        path = var.redis_pv_path
       }
     }
   }
---
+++ persistent_volumes/variables.tf
@@ -0,0 +1,49 @@
+variable "minio_pv_name" {
+  type = string
+  default = "minio-pv"
+}
+
+variable "minio_pv_storage" {
+  type = string
+  default = "2Gi"
+}
+
+variable "minio_pv_access_modes" {
+  type = list(string)
+  default = ["ReadWriteOnce"]
+}
+
+variable "minio_pv_storage_class_name" {
+  type = string
+	default = "hostpath"
+}
+
+variable "minio_pv_path" {
+	type = string
+	default = "/mnt/minio"
+}
+
+variable "redis_pv_name" {
+	type = string
+	default = "redis-pv"
+}
+
+variable "redis_pv_storage" {
+	type = string
+	default = "2Gi"
+}
+
+variable "redis_pv_access_modes" {
+	type = list(string)
+	default = ["ReadWriteOnce"]
+}
+
+variable "redis_pv_storage_class_name" {
+	type = string
+	default = "hostpath"
+}
+
+variable "redis_pv_path" {
+	type = string
+	default = "/mnt/redis"
+}
