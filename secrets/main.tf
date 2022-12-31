d124f43bc82d6b2a39b15646bcb1c69ba3045f92
Jakub Hlavačka <jakubhlavacka123@gmail.com>
2022-12-31 14:56:07 +0100

    refactor minio credentials loading

--- secrets/main.tf
+++ secrets/main.tf
@@ -11,7 +11,7 @@ resource "kubernetes_secret_v1" "minio_secrets" {
 
   # source: https://github.com/hashicorp/terraform-provider-kubernetes/issues/889
   data = {
-    for line in compact(split("\n", file("./secrets/minio_credentials.env"))):
+    for line in compact(split("\r\n", file("./secrets/minio_credentials.env"))):
       split("=",line)[0] => split("=",line)[1]
   }
 }
